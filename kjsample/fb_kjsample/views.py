3d392af558bae2f47c1145a6f031eb0e76a03bd9
kjbyeon <kjbyeon@gmail.com>
2016-08-03 00:13:21 +0900

    kj2

--- kjsample/fb_kjsample/views.py
+++ kjsample/fb_kjsample/views.py
@@ -33,9 +33,9 @@ def post_facebook_message(fbid, recevied_message):
 
     user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
     user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
-#    user_details = requests.get(user_details_url, user_details_params).json()
-#    joke_text = 'Yo '+user_details['first_name']+'..! ' + joke_text
-    joke_text = 'Yo '+'KJ'+'..! ' + joke_text
+    user_details = requests.get(user_details_url, user_details_params).json()
+    joke_text = 'Yo '+user_details['first_name']+'..! ' + joke_text
+#    joke_text = 'Yo '+'KJ'+'..! ' + joke_text
 
 
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
