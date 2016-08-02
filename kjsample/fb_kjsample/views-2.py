3c5f269e00655da0013d49ef3b366d2ca5cfb1ca
kjbyeon <kjbyeon@gmail.com>
2016-08-03 00:02:45 +0900

    kj1

--- kjsample/fb_kjsample/views-2.py
+++ kjsample/fb_kjsample/views-2.py
@@ -1,3 +1,4 @@
+# yomamabot/fb_yomamabot/views.py
 import json, requests, random, re
 from pprint import pprint
 
@@ -7,35 +8,45 @@ from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-# Create your views here.
-PAGE_ACCESS_TOKEN = ""
+#  ------------------------ Fill this with your page access token! -------------------------------
+PAGE_ACCESS_TOKEN = "EAAIyzm0WypQBAA9rp79mlcMav1YwCQISaFDqnDy2QbgFyWEaKy3Lsn8GDQs80E096KGwcmhEP73Y2bJFmZCwyhSwt8oZAbZCkfOHXkW2lrJxWe2mptJ55GG0lQhsgww5NJr2zVhYQJ2z6GhY4POk5KIaaJ3HxS30JhwlyWf4AZDZD"
 VERIFY_TOKEN = "2468101213"
 
-def post_facebook_message(fbid, received_message):
+jokes = {'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
+                    """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
+         'fat': ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""",
+                 """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """],
+         'dumb': ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""",
+                  """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""] }
 
-    print(1, fbid, received_message)
+# Helper function
+def post_facebook_message(fbid, recevied_message):
+    # Remove all punctuations, lower case the text and split it based on space
+    tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
+    joke_text = ''
+    for token in tokens:
+        if token in jokes:
+            joke_text = random.choice(jokes[token])
+            break
+    if not joke_text:
+        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
 
     user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
-    print(2, user_details_url)
-
     user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
-    print(3, user_details_params)
+#    user_details = requests.get(user_details_url, user_details_params).json()
+#    joke_text = 'Yo '+user_details['first_name']+'..! ' + joke_text
+    joke_text = 'Yo '+'KJ'+'..! ' + joke_text
 
-    user_details = requests.get(user_details_url, user_details_params).json()
-    print(4, user_details)
 
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    print(5, post_message_url)
-
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":received_message}})
-    print(6, response_msg)
-
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
+# Create your views here.
 class KjSampleView(generic.View):
     def get(self, request, *args, **kwargs):
-        if self.request.GET['hub.verify_token'] == '2468101213':
+        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
@@ -44,16 +55,16 @@ class KjSampleView(generic.View):
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
 
+    # Post function to handle Facebook messages
     def post(self, request, *args, **kwargs):
         # Converts the text payload into a python dictionary
         incoming_message = json.loads(self.request.body.decode('utf-8'))
-
         # Facebook recommends going through every entry since they might send
         # multiple messages in a single call during high load
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
                 # Check to make sure the received call is a message call
-                # This might be a delivery, optin, postback for other events
+                # This might be delivery, optin, postback for other events
                 if 'message' in message:
                     # Print the message to the terminal
                     pprint(message)
